b230190a26c54e94a01bd73b534ade0000e24cd7
JB2DEV <javier.alca.2000@gmail.com>
2026-01-08 16:43:39 +0100

    add skeleton app with basic domain and ports

--- src/main/java/com/jb2dev/cv/Main.java
+++
@@ -1,7 +0,0 @@
-package com.jb2dev.cv;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/com/jb2dev/cv/domain/profile/ports/ContactInfoQueryPort.java
@@ -0,0 +1,7 @@
+package com.jb2dev.cv.domain.profile.ports;
+
+import com.jb2dev.cv.domain.profile.model.ContactInfo;
+
+public interface ContactInfoQueryPort {
+  ContactInfo getContactInfo();
+}
